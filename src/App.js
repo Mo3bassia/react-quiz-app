8c44b8e3356f3c8e432fcebd2a9ba42957404eb6
Mohamed Abassia <mo3bassia@gmail.com>
2024-10-29 08:16:25 +0300

    fetch data and manage states with useReducer like error, ready and loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import Main from "./Main";
 import Header from "./Header";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import { useEffect, useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Action Unkown");
+  }
+}
 
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, {
+    questions: [],
+    status: "loading",
+  });
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    fetch("https://codingheroes.io/api-react-course-projects/questions.json")
+      .then((result) => result.json())
+      .then((data) => {
+        dispatch({ type: "dataRecieved", payload: data });
+      })
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
