2b31fb5ff2ea3b6980dd9f2000349315017c1f15
Mohamed Abassia <mo3bassia@gmail.com>
2024-11-04 20:42:05 +0200

    add difficulty property for questions

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -3,6 +3,17 @@ function StartScreen({ numQuestions, onStart }) {
     <div className="start">
       <h2>Welcome to the React Quiz!</h2>
       <h3>{numQuestions} questions to test your React mastery</h3>
+      <div className="filter-questions">
+        <label htmlFor="difficulty">
+          <h4>choose the difficulty of the questions</h4>
+        </label>
+        <select id="difficulty">
+          <option selected={true}>All</option>
+          <option>Easy</option>
+          <option>Medium</option>
+          <option>Hard</option>
+        </select>
+      </div>
       <button onClick={onStart} className="btn btn-ui">
         Let's start
       </button>
